3d62f7d17fbfe5ddc4843f5a24de9aa1018d2588
Hiram <doimotxiu12@gmail.com>
2024-10-21 17:04:08 +0700

    push project ms-music on github

---
+++ src/app/ms-header-menu/ms-header-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ms-header-menu',
+  templateUrl: './ms-header-menu.component.html',
+  styleUrls: ['./ms-header-menu.component.css']
+})
+export class MsHeaderMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
