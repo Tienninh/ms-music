13e13d171035802e7e3af67d23d70e4dc3656d12
DESKTOP-FDH0VDG\Admin <doimotxiu12@gmail.com>
2024-10-26 07:41:15 +0700

    update home content

--- src/app/ms-content/ms-content.component.ts
+++ src/app/ms-content/ms-content.component.ts
@@ -9,6 +9,40 @@ export class MsContentComponent implements OnInit {
 
   constructor() { }
 
+  album: any[] = [
+    {
+      id:1,
+      url: "../../assets/public/img/mono.PNG",
+      singer:"MONO",
+      name:"Chăm hoa",
+      author:"",
+      description:"Explore timeless symphonies and discover the masterpieces of classical music.",
+    },
+    {
+      id:2,
+      url: "../../assets/public/img/st.PNG",
+      singer:"SƠN TÙNG M-TP",
+      name:"ĐỪNG LÀM TRÁI TIM ANH ĐAU",
+      author:"",
+      description:"Explore timeless symphonies and discover the masterpieces of classical music.",
+    },
+    {
+      id:3,
+      url: "../../assets/public/img/bru.PNG",
+      singer:"Lady Gaga, Bruno Mars",
+      name:"Die With A Smile",
+      author:"",
+      description:"Explore timeless symphonies and discover the masterpieces of classical music.",
+    },
+    {
+      id:4,
+      url: "../../assets/public/img/apt.PNG",
+      singer:"ROSÉ & Bruno Mars",
+      name:"APT",
+      author:"",
+      description:"Explore timeless symphonies and discover the masterpieces of classical music.",
+    },
+  ]
   ngOnInit() {
   }
 
